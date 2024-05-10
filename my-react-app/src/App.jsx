0b788974bc12b73f79c0ed3c79841883a07f5cb6
Ikramul-m <i20727172@gmail.com>
2024-05-10 15:29:29 +0530

    .

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,7 @@
+import Header from "./assets/Header";
+
 function App() {
-  
+  <Header />;
 }
 
 export default App;
---
+++ my-react-app/src/assets/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header><h1>My Website</h1></header>;
+};
+
+export default Header;
