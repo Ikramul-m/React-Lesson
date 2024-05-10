ba00836c1cb17dc67db4113710c69b8b02675ffb
Ikramul-m <i20727172@gmail.com>
2024-05-10 15:51:07 +0530

    .

--- my-react-app/src/assets/Header.jsx
+++ my-react-app/src/assets/Header.jsx
@@ -2,6 +2,22 @@ const Header = () => {
   return (
     <header>
       <h1>Welcome</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Service</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
