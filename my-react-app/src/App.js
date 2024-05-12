e32c717f90c4301fd772e001e1afb4702b8bad05
Ikramul-m <i20727172@gmail.com>
2024-05-12 21:31:58 +0530

    .

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 
 function App() {
-  function Alert() {
-    alert("Hello world");
-  }
+  // function Alert() {
+  //   alert("Hello world");
+  // }
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <button onClick={Alert}>Click Me</button>
+      <button onClick={() => alert("Ikramul")}>Click Me</button>
     </div>
   );
 }
